9cb3e5715926559220bf3b30cdd93a3916e521ca
Somyaa-Rastogi <120766441+Somyaa-Rastogi@users.noreply.github.com>
2022-12-17 00:00:41 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+      extend: {
+          fontFamily: {
+              base: 'Poppins, sans-serif',
+              krona: '"Krona One", sans-serif',
+          },
+          colors: {
+              dark: '#0C080B',
+              green: '#1E3329',
+              yellow: '#ffc94b',
+              pink: '#fed2d1',
+              'dark-grey': '#B8B8B8',
+              'light-grey': '#ECECEC',
+              light: '#FAFAFA',
+              header: '#E9E6E1',
+          },
+          zIndex: {
+              '-1': -1,
+          },
+          container: {
+              padding: '1rem',
+          },
+      },
+  },
+  variants: {
+      extend: {},
+  },
+  plugins: [],
+};
